53a75e156f2d45c504c0295d974ba6201f699f18
Dragon Black <mdbetico.96@gmail.com>
2022-10-17 01:31:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Buscar y Reemplazar
+
+Actividad con javaFX sobre interfaz para buscar y reemplazar"# BuscarYReemplazar" 
---
+++ src/main/java/dad/byr/Main.java
@@ -0,0 +1,11 @@
+package dad.byr;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BuscarYReemplazar.main(args);
+	}
+
+}
